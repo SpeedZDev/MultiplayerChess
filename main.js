626db236382b2779288b6878a3c398385f70cacb
SpeedZDev <98675043+SpeedZDev@users.noreply.github.com>
2026-02-23 15:48:35 -0500

    Added Moving Pieces to Empty Spaces

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
 
 
 let ChessBoardCells = document.querySelectorAll(".ChessBoardCell")
+let PieceSelected = null;
+
 
 for (let i = 0; i < ChessBoardCells.length; i++) {
-    ChessBoardCells[i].addEventListener("click", PrintID)
+    ChessBoardCells[i].addEventListener("click", function(){
+       
+        
+        if(PieceSelected && this.querySelector(".ChessPiece") === null)
+        {
+            console.log("Moving Piece To " + this.id)
+            MovePiece(this)
+            
+            PieceSelected = null
+
+        }
+        else
+        {
+            SelectPiece(this)
+        }
+    
+    })
+    
     
 }
 
-function PrintID(element)
+function SelectPiece(element)
 {
-    console.log(element)
+    PieceAtLocation = element.querySelector(".ChessPiece")
+    if(PieceAtLocation !== null){
+        PieceSelected = PieceAtLocation
+        console.log("Selected Piece is: " + PieceSelected.src + " At " + element.id)
+    }
+    else{
+         PieceSelected = null
+        console.log("Selected Piece is NULL")
+    }
+}   
+
+function MovePiece(targetCell){
+    targetCell.appendChild(PieceSelected);
 }
